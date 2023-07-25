4d63645805e4a2d87a1481cf2955a38b2e018b3a
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-24 17:18:20 -0700

    initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,13 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			$components: 'src/lib/components',
+			'$components/*': 'src/lib/components/*',
+			$components: 'src/lib/components',
 			'$components/*': 'src/lib/components/*'
 		}
+	},
+	shadcn: {
+		componentPath: './src/lib/components/ui'
 	}
 };
 export default config;
