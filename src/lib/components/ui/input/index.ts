f0d8b1b220c5f290274d10a3db4988ce1c6c6f4e
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-24 23:05:03 -0700

    added additional ui components

---
+++ src/lib/components/ui/input/index.ts
@@ -0,0 +1,4 @@
+export { default as Input } from "./Input.svelte";
+
+export class Label {
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<svelte:head>
-    <title></title>
-</svelte:head>
+<script lang="ts">
+    import { Alert, AlertTitle, AlertDescription } from "$components/ui/alert";
+    import { Button} from "$components/ui/button";
+    import { RadioGroup, RadioGroupItem } from "$components/ui/radio-group";
+    import { Input } from "$components/ui/input";
+    import { Label } from "$components/ui/label";
+    import { Switch } from "$components/ui/switch";
+    import Header from "$components/Header.svelte";
+
+</script>
+
+<div class="flex items-center flex-col gap-8 px-8 pt-8 min-h-screen">
+    <!-- github link and dark theme toggle -->
+
+
+    <!-- Header Text -->
+    <Header />
+    <!-- Sign in Button -->
+    <div class= "flex items-center justify-items-center">
+        <Button class="bg-slate-700 text-white flex flex-col items-center gap-2 px-8 py-12 my-32">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="spotify logo" class="w-36 h-36">
+            <div>Login To View Statistics</div>
+        </Button>
+    </div>
+</div>
