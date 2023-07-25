f4661d6bdad8d2de1425cb0da3b1ebbe1b4236f6
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-25 04:37:35 -0700

    added site footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<div class="flex p-4">
+    <div class="flex-1 flex list-none gap-8 border-box px-4 py-2">
+        <div>
+            <a href="https://github.com/jonathanguven/play-trend" target="_blank">Github</a>
+        </div>
+        <div>
+            <a href="https://www.linkedin.com/in/jonathan-nguyen2004/" target="_blank">LinkedIn</a>
+        </div>
+    </div>
+    <div class="flex-1 flex list-none gap-8 justify-end border-box px-4 py-2">
+        <div>
+            <a href="https://www.linkedin.com/in/jonathan-nguyen2004/" target="_blank">Privacy</a>
+        </div>
+        <div>
+            <a href="https://www.linkedin.com/in/jonathan-nguyen2004/" target="_blank">Terms</a>
+        </div>
+    </div>
+</div>
