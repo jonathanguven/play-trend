b0f6bca69d6903c1a3a97459fd4263ca5cb5b655
Jonathan Nguyen <jonathanguven@gmail.com>
2023-08-23 13:28:19 -0700

    added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import Header from "$components/Header.svelte";
+    import Footer from "$components/Footer.svelte";
+    import Login from "$components/Login.svelte";
+    import { onMount } from 'svelte';
+
+    const client_id = import.meta.env.CLIENT_ID;
+
+
+</script>
+<svelte:head>
+    <title>PlayTrend</title>
+</svelte:head>
+<section>
+    <section class="flex items-center flex-col gap-8 px-8 pt-8">
+        <div>
+            <Header />
+            about page
+        </div>
+
+        <Footer />
+    </section>
+
+</section>
+
+
+
