76f58886178a6b8321195984d852da9c887a5a4f
Jonathan Nguyen <jonathanguven@gmail.com>
2023-08-23 13:29:12 -0700

    removed borders

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,11 +1,34 @@
 <script>
     import { Button } from "$components/ui/button/";
+    import { page } from '$app/stores'
+
+    const id = import.meta.env.VITE_CLIENT_ID;
+    const redirect_uri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI);
+
+    let LOGGED_IN = false;
+
+    const code = $page.url.searchParams.get('code');
+
+    if (code) {
+        LOGGED_IN = true;
+    }
+
+    function loginClick() {
+        console.log(`id: ${id}`);
+        console.log(`redirect: ${redirect_uri}`);
+        window.location.href = `https://accounts.spotify.com/authorize?client_id=${id}&redirect_uri=${redirect_uri}&scope=user-read-private%20user-read-email%20user-modify-playback-state%20user-read-playback-state%20user-top-read%20user-read-recently-played&response_type=code`;
+    }
 </script>
 
 <!-- Sign in Button -->
-<div class="flex flex-col items-center justify-start pd-4 my-32 md:px-0 min-h-full">
-    <Button class="dark text-white flex flex-col items-center gap-2 px-6 py-14">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="spotify logo" class="w-36 h-36">
-        <div class="max-w-[750px] text-lg text-muted-foreground sm:text-md text-center sm:text-left">Login To View Statistics</div>
-    </Button>
-</div>
+{#if !LOGGED_IN}
+    <div class="flex flex-col items-center justify-start pd-4 my-32 md:px-0 min-h-full">
+        <Button on:click={loginClick} class="dark text-white flex flex-col items-center gap-2 px-6 py-14">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="spotify logo" class="w-36 h-36">
+            <div class="max-w-[750px] text-lg text-muted-foreground sm:text-md text-center sm:text-left">Login To View Statistics</div>
+        </Button>
+    </div>
+{:else}
+    <a href="/">Login successful. Click here to go back</a>
+{/if}
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-    import { Alert, AlertTitle, AlertDescription } from "$components/ui/alert";
-    import { Button} from "$components/ui/button";
-    import { RadioGroup, RadioGroupItem } from "$components/ui/radio-group";
-    import { Input } from "$components/ui/input";
-    import { Label } from "$components/ui/label";
-    import { Switch } from "$components/ui/switch";
     import Header from "$components/Header.svelte";
     import Footer from "$components/Footer.svelte";
     import Login from "$components/Login.svelte";
+    import { onMount } from 'svelte';
+
+    const client_id = import.meta.env.CLIENT_ID;
+
 
 </script>
 <svelte:head>
     <title>PlayTrend</title>
 </svelte:head>
-<section class="flex items-center flex-col gap-8 px-8 pt-8">
-    <!-- Header Component -->
-    <div>
-        <Header />
-        <Login />
-    </div>
+<section>
+    <section class="flex items-center flex-col gap-8 px-8 pt-8">
+        <div>
+            <Header />
+            <Login />
+        </div>
+
+        <Footer />
+    </section>
 
 </section>
-<Footer />
 
 
 
