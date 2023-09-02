5023c39191b491ea6221ab24f001b6e6a98534d3
Jonathan <jonathanguven@gmail.com>
2023-09-02 01:14:09 -0700

    official spotify logo

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -9,7 +9,7 @@
 
 <div class="flex flex-col items-center justify-start pd-4 md:px-0 min-h-full mb-72">
     <Button on:click={routeToLogin} class="dark text-white flex flex-col items-center gap-2 px-6 py-14 mt-24">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="spotify logo" class="w-36 h-36">
+        <img src='src\lib\images\Spotify_Logo_RGB_Green.png' alt="spotify logo" class="w-36 h-36">
         <div class="max-w-[750px] text-lg text-muted-foreground sm:text-md text-center sm:text-left">Login To View Statistics</div>
     </Button>
 </div>
